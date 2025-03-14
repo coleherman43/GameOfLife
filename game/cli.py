bf5853c72eb2e5479295e2326697fd823929a7ac
coleherman43 <colesten@gmail.com>
2025-03-13 19:35:56 -0700

    New arguments for number of patterns and randomizing

--- game/cli.py
+++ game/cli.py
@@ -1,11 +1,12 @@
 import argparse
+import random
 
 from game import __version__, patterns, views
 
 def get_command_line_args():
     parser =  argparse.ArgumentParser(
         prog="game",
-        description="Conway's Game of Life in your terminal",
+        description="Conway's Game of Life in your terminal (or window)",
     )
 
     parser.add_argument(
@@ -47,6 +48,22 @@ def get_command_line_args():
         type=int,
         default=7,
         help="frames per second (default: %(default)s)",
+    )
+
+    parser.add_argument(
+        "-n",
+        "--num-patterns",
+        metavar="NUM_PATTERNS",
+        type=int,
+        default=1,
+        help="number of each pattern to add (default: %(default)s)",
+    )
+
+    parser.add_argument(
+        "-r",
+        "--randomize",
+        action="store_true",
+        help="randomize the positions of the patterns",
     )
 
     return parser.parse_args()
